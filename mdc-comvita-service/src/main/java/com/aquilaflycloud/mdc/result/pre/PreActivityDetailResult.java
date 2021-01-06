0cd7466c734b760d76d07b8de8f0495417c547cc
林康琪 <lin.kangqi@aquilaflycloud.com>
2021-01-06 11:55:36 +0800

    完善活动管理接口

--- mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/result/pre/PreActivityDetailResult.java
+++ mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/result/pre/PreActivityDetailResult.java
@@ -1,9 +1,67 @@
 package com.aquilaflycloud.mdc.result.pre;
 
+import com.aquilaflycloud.mdc.enums.pre.ActivityStateEnum;
+import com.aquilaflycloud.mdc.enums.pre.ActivityTypeEnum;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * PreActivityDetailResult
  * @author linkq
  */
+@Data
 public class PreActivityDetailResult {
+
+    @ApiModelProperty(value = "活动名称")
+    private String activityName;
+
+    @ApiModelProperty(value = "活动类型")
+    private ActivityTypeEnum activityType;
+
+    @ApiModelProperty(value = "状态（1-未开始 2-进行中 3-已结束 4-已下架）")
+    private ActivityStateEnum activityState;
+
+
+    @ApiModelProperty(value = "活动标签名")
+    private List<String> folksonomyNames;
+
+    /**
+     * 活动开始时间
+     */
+    @ApiModelProperty(value = "活动开始时间")
+    private Date beginTime;
+
+    /**
+     * 活动结束时间
+     */
+    @ApiModelProperty(value = "活动结束时间")
+    private Date endTime;
+
+    /**
+     * 关联商品
+     */
+    @ApiModelProperty(value = "商品编号")
+    private String refGoodsCode;
+
+    /**
+     * 关联的销售规则
+     */
+    @ApiModelProperty(value = "销售规则名")
+    private String refRuleName;
+
+    /**
+     * 活动照片
+     */
+    @ApiModelProperty(value = "活动照片")
+    private String activityPicture;
+
+    /**
+     * 活动介绍
+     */
+    @ApiModelProperty(value = "活动介绍")
+    private String activityProfile;
 
 }
--- mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/service/impl/PreActivityServiceImpl.java
+++ mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/service/impl/PreActivityServiceImpl.java
@@ -4,15 +4,22 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
 import com.aquilaflycloud.mdc.enums.member.BusinessTypeEnum;
+import com.aquilaflycloud.mdc.mapper.FolksonomyBusinessRelMapper;
 import com.aquilaflycloud.mdc.mapper.PreActivityInfoMapper;
+import com.aquilaflycloud.mdc.mapper.PreGoodsInfoMapper;
+import com.aquilaflycloud.mdc.mapper.PreRuleInfoMapper;
+import com.aquilaflycloud.mdc.model.folksonomy.FolksonomyBusinessRel;
 import com.aquilaflycloud.mdc.model.folksonomy.FolksonomyInfo;
 import com.aquilaflycloud.mdc.model.pre.PreActivityInfo;
+import com.aquilaflycloud.mdc.model.pre.PreGoodsInfo;
+import com.aquilaflycloud.mdc.model.pre.PreRuleInfo;
 import com.aquilaflycloud.mdc.param.pre.*;
 import com.aquilaflycloud.mdc.result.pre.PreActivityAnalysisResult;
 import com.aquilaflycloud.mdc.result.pre.PreActivityDetailResult;
 import com.aquilaflycloud.mdc.service.FolksonomyService;
 import com.aquilaflycloud.mdc.service.PreActivityService;
 import com.aquilaflycloud.mdc.util.MdcUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.gitee.sop.servercommon.exception.ServiceException;
@@ -20,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -35,19 +43,35 @@ public class PreActivityServiceImpl implements PreActivityService {
     private PreActivityInfoMapper preActivityInfoMapper;
 
     @Resource
+    private PreRuleInfoMapper preRuleInfoMapper;
+
+    @Resource
+    private PreGoodsInfoMapper preGoodsInfoMapper;
+
+    @Resource
+    private FolksonomyBusinessRelMapper folksonomyBusinessRelMapper;
+
+    @Resource
     private FolksonomyService folksonomyService;
 
     @Override
     public IPage<PreActivityInfo> page(PreActivityPageParam param) {
         List<Long> folksonomyIds = param.getFolksonomyIds();
+        List<Long> businessIds = new ArrayList<>();
         if(CollUtil.isNotEmpty(folksonomyIds)){
-
+            QueryWrapper<FolksonomyBusinessRel> qw = new QueryWrapper<>();
+            qw.in("folksonomy_id", folksonomyIds);
+            List<FolksonomyBusinessRel> folksonomyBusinessRels = folksonomyBusinessRelMapper.selectList(qw);
+            if(CollUtil.isNotEmpty(folksonomyBusinessRels)){
+                folksonomyBusinessRels.forEach(f -> {
+                    businessIds.add(f.getBusinessId());
+                });
+            }
         }
-//        List<FolksonomyInfo> folksonomyInfos = folksonomyService.getFolksonomyBusinessList(BusinessTypeEnum.PREACTIVITY);
 
         IPage<PreActivityInfo> list = preActivityInfoMapper.selectPage(param.page(), Wrappers.<PreActivityInfo>lambdaQuery()
                 .like( param.getActivityName()!=null,PreActivityInfo::getActivityName, param.getActivityName())
-//                .like( param.getFolksonomyId()!=null,PreActivityInfo::getFolksonomyId, param.getFolksonomyId())
+                .in(CollUtil.isNotEmpty(businessIds),PreActivityInfo::getId,businessIds)
                 .eq( param.getActivityState()!=null,PreActivityInfo::getActivityState, param.getActivityState().getType())
                 .eq( param.getActivityType()!=null,PreActivityInfo::getActivityType, param.getActivityType().getType())
                 .apply(param.getCreateTimeStart() != null,"date_format (optime,'%Y-%m-%d') >= date_format('" + param.getCreateTimeStart() + "','%Y-%m-%d')")
@@ -67,7 +91,6 @@ public class PreActivityServiceImpl implements PreActivityService {
         activityInfo.setId(MdcUtil.getSnowflakeId());
         int count = preActivityInfoMapper.insert(activityInfo);
         if (count == 1) {
-            //保存业务功能标签
             folksonomyService.saveFolksonomyBusinessRel(BusinessTypeEnum.PREACTIVITY, activityInfo.getId(), param.getFolksonomyIds());
             log.info("新增活动成功");
         } else {
@@ -131,15 +154,25 @@ public class PreActivityServiceImpl implements PreActivityService {
         PreActivityInfo info=  preActivityInfoMapper.selectById(param.getId());
         PreActivityDetailResult preActivityDetailResult = new PreActivityDetailResult();
         BeanUtil.copyProperties(info, preActivityDetailResult);
-        //@TODO
-//        if(StringUtils.isNotBlank(info.getFolksonomyId())){
-//            String[] tagId=info.getFolksonomyId().split(",");
-//            List<Long> list=new ArrayList<>();
-//            for(String id:tagId){
-//                list.add(Long.parseLong(id));
-//            }
-//            returnGoodsInfoParam.setFolksonomyIds(list);
-//        }
+        //refGoodsCode
+        PreGoodsInfo goods = preGoodsInfoMapper.selectById(info.getRefGoods());
+        if(null != goods){
+            preActivityDetailResult.setRefGoodsCode(goods.getGoodsCode());
+        }
+        //refRuleName
+        PreRuleInfo rule = preRuleInfoMapper.selectById(info.getRefRule());
+        if(null != rule){
+            preActivityDetailResult.setRefRuleName(rule.getRuleName());
+        }
+        // ksonomyIds
+        List<FolksonomyInfo> folksonomyInfos = folksonomyService.getFolksonomyBusinessList(BusinessTypeEnum.PREACTIVITY, info.getId());
+        if(CollUtil.isNotEmpty(folksonomyInfos)){
+            List<String> names = new ArrayList<>();
+            folksonomyInfos.forEach(s -> {
+                names.add(s.getName());
+            });
+            preActivityDetailResult.setFolksonomyNames(names);
+        }
         return preActivityDetailResult;
     }
 
