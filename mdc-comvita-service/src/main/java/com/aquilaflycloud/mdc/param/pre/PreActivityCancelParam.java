3bfd5976a9bcf6be24be62c950ef021639423186
林康琪 <lin.kangqi@aquilaflycloud.com>
2021-01-07 18:51:52 +0800

    活动接口参数修正

--- mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/param/pre/PreActivityCancelParam.java
+++ mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/param/pre/PreActivityCancelParam.java
@@ -19,6 +19,6 @@ public class PreActivityCancelParam {
     private Long id;
 
     @ApiModelProperty(value = "活动状态(pre.ActivityStateEnum)")
-    private ActivityStateEnum activityState = ActivityStateEnum.CANCELED;
+    private ActivityStateEnum activityState;
 
 }
