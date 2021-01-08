2904f31ae556a7257136d1d49789536083837c3a
pengyongliang <peng.yongliang@aquilaflycloud.com>
2021-01-08 15:58:25 +0800

    确认订单，不通过时加入字段 不通过标识

---
+++ mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/enums/pre/FailSymbolEnum.java
@@ -0,0 +1,36 @@
+package com.aquilaflycloud.mdc.enums.pre;
+
+import com.alibaba.fastjson.annotation.JSONType;
+import com.aquilaflycloud.dataAuth.component.EnumDeserializer;
+import com.baomidou.mybatisplus.annotation.EnumValue;
+
+/**
+ * @Author pengyongliang
+ * @Date 2021/1/8 15:52
+ * @Version 1.0
+ */
+@JSONType(serializeEnumAsJavaBean = true, deserializer = EnumDeserializer.class)
+public enum FailSymbolEnum {
+
+    // 是否为不通过 0.是 1.否
+    YES(0, "是"),
+    NO(1, "否");
+
+    FailSymbolEnum(int type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    @EnumValue
+    private final int type;
+
+    private final String name;
+
+    public int getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
