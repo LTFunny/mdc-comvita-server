e5c7b3a01512e27dfa222dbc43bc6b40ca94d82b
黄星 <huang.xing@aquilaflycloud.com>
2021-01-09 10:43:30 +0800

    优化小程序二维码生成方法

--- mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/controller/WechatMiniController.java
+++ mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/controller/WechatMiniController.java
@@ -1,23 +1,15 @@
 package com.aquilaflycloud.mdc.controller;
 
-import cn.hutool.core.util.StrUtil;
 import com.aquilaflycloud.dataAuth.common.BaseResult;
-import com.aquilaflycloud.mdc.extra.wechat.service.WechatMiniService;
-import com.aquilaflycloud.mdc.param.wechat.MiniQrcodeAddParam;
-import com.aquilaflycloud.mdc.util.MdcUtil;
-import com.aquilaflycloud.util.AliOssUtil;
+import com.aquilaflycloud.mdc.param.wechat.MiniQrcodeGetParam;
+import com.aquilaflycloud.mdc.service.WechatMiniFunctionService;
 import com.gitee.sop.servercommon.annotation.ApiMapping;
-import com.gitee.sop.servercommon.exception.ServiceException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 /**
  * WechatMiniController
@@ -30,28 +22,13 @@ import java.io.FileNotFoundException;
 public class WechatMiniController {
 
     @Resource
-    private WechatMiniService wechatMiniService;
+    private WechatMiniFunctionService wechatMiniFunctionService;
 
     @ApiOperation(value = "根据内容生成小程序二维码", notes = "根据内容生成小程序二维码")
 //    @PreAuthorize("hasAuthority('mdc:wechat:qrcode')")
     @ApiMapping(value = "backend.comvita.wechat.qrcode.add", method = RequestMethod.POST, permission = true)
-    public BaseResult<String> addQrcode(MiniQrcodeAddParam param) {
-        try {
-            File file = wechatMiniService.getWxMaServiceByAppId(param.getAppId()).getQrcodeService().createQrcode(param.getPath(), param.getWidth());
-            //上传文件至服务器并返回url
-            try {
-                String path = param.getAppId() + "/" + StrUtil.split(param.getPath(), "?")[0].replace("/", ".");
-                String name = MdcUtil.getSnowflakeIdStr();
-                String url = AliOssUtil.uploadFile(path, StrUtil.appendIfMissing(name, ".png"), new FileInputStream(file), AliOssUtil.MEMBER_STYLE);
-                return BaseResult.buildResult(url);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                throw new ServiceException("上传小程序二维码失败");
-            }
-        } catch (WxErrorException e) {
-            e.printStackTrace();
-            throw new ServiceException(String.valueOf(e.getError().getErrorCode()), e.getError().getErrorMsg());
-        }
+    public BaseResult<String> addQrcode(MiniQrcodeGetParam param) {
+        return wechatMiniFunctionService.getMiniQrcode(param);
     }
 
 }
--- mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/param/wechat/MiniQrcodeGetParam.java
+++ mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/param/wechat/MiniQrcodeGetParam.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 @Accessors(chain = true)
-public class MiniQrcodeAddParam {
+public class MiniQrcodeGetParam {
     @ApiModelProperty(value = "微信appId", required = true)
     @NotBlank(message = "微信appId不能为空")
     private String appId;
