9a5b5dbfcf90fedd6e7e9f5c617f524fd60e6dca
黄星 <huang.xing@aquilaflycloud.com>
2021-03-05 16:41:06 +0800

    新增相关实体类mapper

---
+++ mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/mapper/PreActivityQrCodeInfoMapper.java
@@ -0,0 +1,7 @@
+package com.aquilaflycloud.mdc.mapper;
+
+import com.aquilaflycloud.mdc.model.pre.PreActiveQrCodeInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface PreActivityQrCodeInfoMapper extends BaseMapper<PreActiveQrCodeInfo> {
+}
---
+++ mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/mapper/PreFlashOrderInfoMapper.java
@@ -0,0 +1,7 @@
+package com.aquilaflycloud.mdc.mapper;
+
+import com.aquilaflycloud.mdc.model.pre.PreFlashOrderInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface PreFlashOrderInfoMapper extends BaseMapper<PreFlashOrderInfo> {
+}
