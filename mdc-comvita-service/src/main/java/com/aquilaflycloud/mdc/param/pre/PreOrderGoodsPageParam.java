36e16576f1110fadaa88c3378477bab7f2ad9836
黄星 <huang.xing@aquilaflycloud.com>
2021-03-15 16:28:27 +0800

    预约自提列表返回预售订单明细(商品列表)

--- mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/param/pre/PreOrderGoodsPageParam.java
+++ mdc-comvita-service/src/main/java/com/aquilaflycloud/mdc/param/pre/PreOrderGoodsPageParam.java
@@ -2,9 +2,10 @@ package com.aquilaflycloud.mdc.param.pre;
 
 import com.aquilaflycloud.dataAuth.common.PageParam;
 import com.aquilaflycloud.mdc.enums.pre.OrderGoodsStateEnum;
-import com.aquilaflycloud.mdc.enums.pre.PickingCardStateEnum;
+import com.aquilaflycloud.mdc.model.pre.PreOrderGoods;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotNull;
 
@@ -13,8 +14,9 @@ import javax.validation.constraints.NotNull;
  * @Date 2020/12/31 11:40
  * @Version 1.0
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class PreOrderGoodsPageParam extends PageParam {
+public class PreOrderGoodsPageParam extends PageParam<PreOrderGoods> {
 
     @ApiModelProperty(value = "订单商品状态", required = true)
     @NotNull(message = "订单商品状态不能为空")
